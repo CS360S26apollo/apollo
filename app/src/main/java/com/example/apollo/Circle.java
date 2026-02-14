59c5f263261db520a2ad58054890604ec0105c03
AbdullahKhaliq12 <27100400@lums.edu.pk>
2026-02-15 01:07:32 +0500

    Add Circle Shape

---
+++ app/src/main/java/com/example/apollo/Circle.java
@@ -0,0 +1,10 @@
+package com.example.apollo;
+
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+}
